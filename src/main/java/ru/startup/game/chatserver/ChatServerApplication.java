ba24e3d68272cd9d4f4bbb5a0b75ddafe40bebc2
Pushkin Sergey <pushkinsergey.voronezh@gmail.com>
2020-03-02 23:01:41 +0300

    Initial commit

---
+++ src/main/java/ru/startup/game/chatserver/ChatServerApplication.java
@@ -0,0 +1,13 @@
+package ru.startup.game.chatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/startup/game/chatserver/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.startup.game.chatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
