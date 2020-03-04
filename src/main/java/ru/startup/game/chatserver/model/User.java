014a8b6a6ee785b908cba7cd22e856f231e6521f
Pushkin Sergey <pushkinsergey.voronezh@gmail.com>
2020-03-04 18:05:42 +0300

    Fix database connection

--- src/main/java/ru/startup/game/chatserver/model/User.java
+++ src/main/java/ru/startup/game/chatserver/model/User.java
@@ -3,18 +3,21 @@ package ru.startup.game.chatserver.model;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table
 @Data
 @RequiredArgsConstructor
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String userName;
 
+    @Column
     private String firstName;
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS public.user (
   id          INTEGER PRIMARY KEY,
   user_name   VARCHAR NOT NULL,
   first_name  VARCHAR NOT NULL
